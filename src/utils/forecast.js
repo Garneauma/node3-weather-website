8e200f93f5859b9b18665387cc92c4ccd6189689
Marc-André <garneauma@gmail.com>
2020-05-01 13:30:10 -0400

    Added pressure to forecast string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,13 +5,12 @@ const forecast = (longitude, latitude, callback) => {
     const url = 'http://api.openweathermap.org/data/2.5/weather?lon=' + encodeURIComponent(longitude) + '&lat=' + encodeURIComponent(latitude) + '&units=Metric&appid=' + key
 
     request({ url, json: true }, (error, {body}) => {
-        //console.log(response.body)
         if (error) {
             callback('Unable to connect to weather services.', undefined)
         } else if (body.message) {
             callback('Unable to find location. Try again with a different search term.', undefined)
         } else {
-            callback(undefined, body.weather[0].main + '. It is currently ' + Math.round(body.main.temp) + ' degrees out. It feels like ' + Math.round(body.main.feels_like) + ' degrees.')
+            callback(undefined, body.weather[0].main + '. It is currently ' + Math.round(body.main.temp) + ' degrees out. It feels like ' + Math.round(body.main.feels_like) + ' degrees. Pressure is ' + body.main.pressure + ' hPa')
         }
     })
 }
